1e994710c8686cdb747c63fe20b515e0691e7df9
Kerem Tazedal <kerem.tazedal.89@gmail.com>
2024-03-25 19:06:51 +0100

    Update OrderRepo.java

--- src/main/java/se/dmitrykhalizov/webbshoplabb/repository/OrderRepo.java
+++ src/main/java/se/dmitrykhalizov/webbshoplabb/repository/OrderRepo.java
@@ -1,7 +1,7 @@
 package se.dmitrykhalizov.webbshoplabb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se.dmitrykhalizov.webbshoplabb.database.Order;
+import se.dmitrykhalizov.webbshoplabb.entity.Order;
 
 public interface OrderRepo extends JpaRepository <Order, Integer> {
 }
