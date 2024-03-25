6140bdc86409d1512b041b13956297ef219ebd10
Kerem Tazedal <kerem.tazedal.89@gmail.com>
2024-03-25 19:06:53 +0100

    Update UserRepo.java

--- src/main/java/se/dmitrykhalizov/webbshoplabb/repository/UserRepo.java
+++ src/main/java/se/dmitrykhalizov/webbshoplabb/repository/UserRepo.java
@@ -1,7 +1,7 @@
 package se.dmitrykhalizov.webbshoplabb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se.dmitrykhalizov.webbshoplabb.database.User;
+import se.dmitrykhalizov.webbshoplabb.entity.User;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
     boolean existsUserByUsernameAndPassword(String username, String password);
