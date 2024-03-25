2bba6e8dca93ccdb0e7cecfced036edae9a129f9
Kerem Tazedal <kerem.tazedal.89@gmail.com>
2024-03-25 19:07:24 +0100

    Create EnumSelection.java

---
+++ src/main/java/se/dmitrykhalizov/webbshoplabb/entity/EnumSelection.java
@@ -0,0 +1,12 @@
+package se.dmitrykhalizov.webbshoplabb.entity;
+
+public enum EnumSelection {
+    customer,
+    admin,
+    active,
+    discountinued,
+    pending,
+    shipped,
+    delivered,
+
+}
