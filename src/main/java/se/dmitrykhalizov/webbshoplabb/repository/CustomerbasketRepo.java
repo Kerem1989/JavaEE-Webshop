bea0cd6c67798e4e14bf9555b1a2f66b2d7197ea
Kerem Tazedal <kerem.tazedal.89@gmail.com>
2024-03-25 19:06:45 +0100

    Update CustomerbasketRepo.java

--- src/main/java/se/dmitrykhalizov/webbshoplabb/repository/CustomerbasketRepo.java
+++ src/main/java/se/dmitrykhalizov/webbshoplabb/repository/CustomerbasketRepo.java
@@ -1,7 +1,7 @@
 package se.dmitrykhalizov.webbshoplabb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import se.dmitrykhalizov.webbshoplabb.database.Customerbasket;
+import se.dmitrykhalizov.webbshoplabb.entity.Customerbasket;
 
 public interface CustomerbasketRepo extends JpaRepository <Customerbasket, Integer> {
 }
